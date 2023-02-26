7bee8b49db84bba5be489b7750611e5e20438feb
Rohit Talreja <=>
2023-02-26 15:50:47 +0530

    Read operation done

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+    @artice = Article.new
+  end
+  def create
+    @article = Article.new(title: "..." , body: "...")
+
+    if @artice.save
+      redirect_to @article
+    else
+      render :new,status: :unprocessable_entity
+    end
+  end
+end
